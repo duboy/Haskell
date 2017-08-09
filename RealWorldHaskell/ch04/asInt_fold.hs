fb4de6882fecb2d35b41e0eec7688036c89d9c6f
duboy <futuredby@gmail.com>
2017-08-09 10:44:32 +0800

    asInt_fold.hs failed to add foldr

--- RealWorldHaskell/ch04/asInt_fold.hs
+++ RealWorldHaskell/ch04/asInt_fold.hs
@@ -2,6 +2,14 @@
 -- exercise
 import Data.Char (digitToInt)
 
-asInt_fold :: String -> Int
-asInt_fold xs = foldl step 0 xs
+asInt_foldl :: String -> Int
+
+asInt_foldl ('-' : xs) = -1 * asInt_foldl xs
+asInt_foldl xs = foldl step 0 xs
+  where step acc xs = acc * 10 + digitToInt xs
+
+asInt_foldr :: String -> Int
+
+asInt_foldr ('-' : xs) = -1 * asInt_foldr xs
+asInt_foldr xs = foldr step 0 xs
   where step acc xs = acc * 10 + digitToInt xs
